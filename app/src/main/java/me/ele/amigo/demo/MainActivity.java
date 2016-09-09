3c4182e5340cab56fb15756d0a7f057b38199807
wwm <wwm@wwmdeMacBook-Pro.local>
2016-09-09 18:01:31 +0800

    update lib usage

--- app/src/main/java/me/ele/amigo/demo/MainActivity.java
+++ app/src/main/java/me/ele/amigo/demo/MainActivity.java
@@ -2,14 +2,17 @@ package me.ele.amigo.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+
+import java.io.File;
+
+import me.ele.amigo.Amigo;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -41,32 +44,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Main2Activity.class));
             }
         });
-    }
 
+        findViewById(R.id.apply_patch_apk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                applyPatchApk();
+            }
+        });
+    }
 
     private void crash() {
-        int a = 1 / 0;
+        throw new RuntimeException("mock crash");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void applyPatchApk() {
+        File fixedApkFile = new File(Environment.getExternalStorageDirectory(), "demo.apk");
+        if (fixedApkFile.exists()) {
+            Amigo.work(this, fixedApkFile);
+//             Amigo.workLater(this, fixedApkFile);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
