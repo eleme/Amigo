7ffd525fcef56a13996771c8a3474095ae4f1c38
peacepassion <daqi1990@gmail.com>
2016-09-27 11:17:35 +0800

    test asset support and asset is support perfectly

--- app/src/patch/java/me/ele/demo/A.java
+++ app/src/patch/java/me/ele/demo/A.java
@@ -2,10 +2,31 @@ package me.ele.demo;
 
 import me.ele.app.amigo.BuildConfig;
 
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class A {
 
     public static String getDes() {
-        return "Patch Content, version code: " + BuildConfig.VERSION_CODE;
+        return "Patch, version code: " + BuildConfig.VERSION_CODE;
     }
 
+    public static String loadAsset(Context context) {
+        try {
+            // load the same entry
+//            InputStream is = context.getAssets().open("a.txt");
+            // load new entry
+            InputStream is = context.getAssets().open("b.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line = reader.readLine();
+            is.close();
+            return  line;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Loading Error";
+    }
 }
