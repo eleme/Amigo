f097cb7412e776f9e066bf39dd8f0edd4634074f
wwm <wwm0609@gmail.com>
2017-03-06 17:26:29 +0800

    fix #attachBaseContext not delegate to real application

--- amigo-lib/src/main/java/me/ele/amigo/PatchInfoProvider.java
+++ amigo-lib/src/main/java/me/ele/amigo/PatchInfoProvider.java
@@ -1,147 +1,38 @@
 package me.ele.amigo;
 
-import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.net.Uri;
 import android.text.TextUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by wwm on 11/23/16.
  */
 
-public class PatchInfoProvider extends ContentProvider {
+public class PatchInfoProvider {
 
-    private static final String SP_NAME = "Amigo";
     private static final String WORKING_PATCH_APK_CHECKSUM = "working_patch_apk_checksum";
-
-    SharedPreferences sharedPreferences = null;
-
     private static final String PARAM_KEY_CHECKSUM = "checksum";
 
-    @Override
-    public boolean onCreate() {
-        sharedPreferences = getContext().getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
-        return true;
-    }
+    private Context context;
 
-    @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-                        String sortOrder) {
-
-        String path = uri.getLastPathSegment();
-
-        if ("query_working_checksum".equals(path)) {
-            String result = queryWorkingChecksum();
-            MatrixCursor matrixCursor = new MatrixCursor(new String[]{PARAM_KEY_CHECKSUM}, 1);
-            matrixCursor.addRow(new Object[]{result});
-            return matrixCursor;
-        }
-
-        Map<String, String> params = parseQuery(uri.getEncodedQuery());
-        String checksum = params.get(PARAM_KEY_CHECKSUM);
-        if ("is_dex_optimized".equals(path)) {
-            boolean optimized = isDexFileOptimized(checksum);
-            MatrixCursor matrixCursor = new MatrixCursor(new String[]{"optimized"}, 1);
-            matrixCursor.addRow(new Object[]{optimized ? 1 : 0});
-            return matrixCursor;
-        }
-
-        if ("query_patch_file_checksum_map".equals(path)) {
-            String json = queryPatchChecksumMap(checksum);
-            MatrixCursor matrixCursor = new MatrixCursor(new String[]{"json_map"}, 1);
-            matrixCursor.addRow(new Object[]{json});
-            return matrixCursor;
-        }
-
-        return null;
-    }
-
-    @Override
-    public String getType(Uri uri) {
-        return null;
-    }
-
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        return null;
-    }
-
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
-    }
-
-    @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        String lastPathSegment = uri.getLastPathSegment();
-        Map<String, String> params = parseQuery(uri.getEncodedQuery());
-
-        if ("clear_all".equals(lastPathSegment)) {
-            sharedPreferences.edit().clear().commit();
-            return 0;
-        }
-
-        String checksum = params.get(PARAM_KEY_CHECKSUM);
-        if ("set_working_checksum".equals(lastPathSegment)) {
-            return updateWorkingChecksum(checksum) ? 1 : 0;
-        }
-
-        if ("set_dex_optimized".equals(lastPathSegment)) {
-            boolean optimized = Boolean.parseBoolean(params.get("optimized"));
-            return setDexFileOptimized(checksum, optimized) ? 1 : 0;
-        }
-
-        if ("update_patch_file_checksum_map".equals(lastPathSegment)) {
-            String map = params.get("map");
-            return updatePatchChecksumMap(checksum, map) ? 1 : 0;
-        }
-
-        return 0;
-    }
-
-    private boolean updateWorkingChecksum(String newChecksum) {
-        sharedPreferences.edit()
-                .putString(WORKING_PATCH_APK_CHECKSUM, newChecksum).commit();
-        return true;
-    }
-
-    private String queryWorkingChecksum() {
-        return sharedPreferences.getString(WORKING_PATCH_APK_CHECKSUM, "");
-    }
-
-    private boolean isDexFileOptimized(String checksum) {
-        return sharedPreferences.getBoolean(checksum, false);
-    }
-
-    private boolean setDexFileOptimized(String checksum, boolean optimized) {
-        if (TextUtils.isEmpty(checksum)) {
-            return false;
-        }
-
-        sharedPreferences.edit().putBoolean(checksum, optimized).commit();
-        return true;
-    }
-
-    private String queryPatchChecksumMap(String checksum) {
-        return sharedPreferences.getString("patch_checksum_map_" + checksum, "");
-    }
-
-    private boolean updatePatchChecksumMap(String checksum, String jsonMap) {
-        if (TextUtils.isEmpty(checksum)) {
-            return false;
-        }
-
-        sharedPreferences.edit().putString("patch_checksum_map_" + checksum, jsonMap).commit();
-        return true;
+    public PatchInfoProvider(Context context) {
+        this.context = context;
     }
 
     private static Map<String, String> parseQuery(String query) {
@@ -154,15 +45,213 @@ public class PatchInfoProvider extends ContentProvider {
         String[] pairs = query.split("&");
         for (String pair : pairs) {
             int idx = pair.indexOf("=");
-            if (idx != -1)
+            if (idx != -1) {
                 try {
-                    queryMap.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder
-                            .decode
-                                    (pair.substring(idx + 1), "UTF-8"));
+                    queryMap.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"),
+                            URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
+            }
         }
         return queryMap;
+    }
+
+    public Cursor query(Uri uri) {
+        String command = uri.getLastPathSegment();
+        if ("query_working_checksum".equals(command)) {
+            String result = queryWorkingChecksum();
+            MatrixCursor matrixCursor = new MatrixCursor(new String[] {PARAM_KEY_CHECKSUM}, 1);
+            matrixCursor.addRow(new Object[] {result});
+            return matrixCursor;
+        }
+
+        Map<String, String> params = parseQuery(uri.getQuery());
+        String checksum = params.get(PARAM_KEY_CHECKSUM);
+        if ("is_dex_optimized".equals(command)) {
+            boolean optimized = isDexFileOptimized(checksum);
+            MatrixCursor matrixCursor = new MatrixCursor(new String[] {"optimized"}, 1);
+            matrixCursor.addRow(new Object[] {optimized ? 1 : 0});
+            return matrixCursor;
+        }
+
+        if ("query_patch_file_checksum_map".equals(command)) {
+            String json = queryPatchChecksumMap(checksum);
+            MatrixCursor matrixCursor = new MatrixCursor(new String[] {"json_map"}, 1);
+            matrixCursor.addRow(new Object[] {json});
+            return matrixCursor;
+        }
+
+        return null;
+    }
+
+    public int update(Uri uri, ContentValues values) {
+        String command = uri.getLastPathSegment();
+        Map<String, String> params = parseQuery(uri.getEncodedQuery());
+        if ("clear_all".equals(command)) {
+            clearLocked();
+            return 0;
+        }
+
+        String checksum = params.get(PARAM_KEY_CHECKSUM);
+        if ("set_working_checksum".equals(command)) {
+            return updateWorkingChecksum(checksum) ? 1 : 0;
+        }
+
+        if ("set_dex_optimized".equals(command)) {
+            boolean optimized = Boolean.parseBoolean(params.get("optimized"));
+            return setDexFileOptimized(checksum, optimized) ? 1 : 0;
+        }
+
+        if ("update_patch_file_checksum_map".equals(command)) {
+            String map = values.getAsString("checkSumMap");
+            return updatePatchChecksumMap(checksum, map) ? 1 : 0;
+        }
+
+        return 0;
+    }
+
+    private boolean updateWorkingChecksum(String newChecksum) {
+        writeLocked(WORKING_PATCH_APK_CHECKSUM, newChecksum);
+        return true;
+    }
+
+    private String queryWorkingChecksum() {
+        return String.valueOf(readLocked(WORKING_PATCH_APK_CHECKSUM));
+    }
+
+    private boolean isDexFileOptimized(String checksum) {
+        return Boolean.valueOf(String.valueOf(readLocked(checksum)));
+    }
+
+    private boolean setDexFileOptimized(String checksum, boolean optimized) {
+        if (TextUtils.isEmpty(checksum)) {
+            return false;
+        }
+        return writeLocked(checksum, optimized);
+    }
+
+    private String queryPatchChecksumMap(String checksum) {
+        return String.valueOf(readLocked("patch_checksum_map_" + checksum));
+    }
+
+    private boolean updatePatchChecksumMap(String newChecksum, String jsonMap) {
+        if (TextUtils.isEmpty(newChecksum)) {
+            return false;
+        }
+
+        return writeLocked("patch_checksum_map_" + newChecksum, jsonMap);
+    }
+
+    /*
+     in order to sharing data between the main process and load patch process, we have to make sure
+     the two of them don't modify the underlying file concurrently and ensure the changes made by
+     any of them are visible to the other
+    */
+    private void clearLocked() {
+        synchronized (PatchInfoProvider.class) {
+            FileLock lock = null;
+            RandomAccessFile raf = null;
+            try {
+                File file = getPatchInfoFile();
+                if (!file.exists()) {
+                    return;
+                }
+                raf = new RandomAccessFile(file, "rw");
+                FileChannel channel = raf.getChannel();
+                lock = channel.lock();
+                raf.setLength(0);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (lock != null) {
+                        lock.release();
+                    }
+                    if (raf != null) {
+                        raf.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private boolean writeLocked(String key, Object value) {
+        synchronized (PatchInfoProvider.class) {
+            FileLock lock = null;
+            RandomAccessFile raf = null;
+            try {
+                File file = getPatchInfoFile();
+                raf = new RandomAccessFile(file, "rw");
+                FileChannel channel = raf.getChannel();
+                lock = channel.lock();
+                byte[] content = new byte[(int) raf.length()];
+                raf.read(content);
+                JSONObject object = content.length > 0 ? new JSONObject(new String(content))
+                        : new JSONObject();
+                object.put(key, value);
+                raf.setLength(0);
+                raf.write(object.toString().getBytes("UTF-8"));
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (lock != null) {
+                        lock.release();
+                    }
+                    if (raf != null) {
+                        raf.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return false;
+        }
+    }
+
+    private Object readLocked(String key) {
+        synchronized (PatchInfoProvider.class) {
+            FileLock lock = null;
+            RandomAccessFile raf = null;
+            try {
+                File file = getPatchInfoFile();
+                raf = new RandomAccessFile(file, "rw");
+                FileChannel channel = raf.getChannel();
+                lock = channel.lock();
+                int contentLength = (int) raf.length();
+                if (contentLength == 0) {
+                    return null;
+                }
+                byte[] content = new byte[contentLength];
+                raf.read(content);
+                return new JSONObject(new String(content)).optString(key);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (lock != null) {
+                        lock.release();
+                    }
+                    if (raf != null) {
+                        raf.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
+
+    private File getPatchInfoFile() {
+        return AmigoDirs.getInstance(context).patchInfoFile();
     }
 }
--- amigo-lib/src/main/java/me/ele/amigo/PatchInfoUtil.java
+++ amigo-lib/src/main/java/me/ele/amigo/PatchInfoUtil.java
@@ -4,14 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
-import android.text.TextUtils;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by wwm on 11/25/16.
@@ -19,28 +18,21 @@ import java.util.Map;
 
 public class PatchInfoUtil {
 
-    private static String URI_PREFIX = "";
-
     public static boolean setWorkingChecksum(Context context, String checksum) {
-        ensureProviderAuthroties(context);
-
-        return context.getContentResolver().update(Uri.parse(URI_PREFIX +
-                        "set_working_checksum?checksum=" + checksum),
-                new ContentValues(0), null, null) > 0;
+        return getProvider(context).update(
+                Uri.parse(
+                        "content://patch_info_provider/set_working_checksum?checksum=" + checksum),
+                null) > 0;
     }
 
-    private static void ensureProviderAuthroties(Context context) {
-        if (TextUtils.isEmpty(URI_PREFIX)) {
-            URI_PREFIX = String.format("content://%s.amigo_patch_info_provider/",
-                    context.getPackageName());
-        }
+    private static PatchInfoProvider getProvider(Context context) {
+        return new PatchInfoProvider(context);
     }
 
     public static String getWorkingChecksum(Context context) {
-        ensureProviderAuthroties(context);
-        Cursor cursor = context.getContentResolver().query(Uri.parse
-                        (URI_PREFIX + "query_working_checksum"), null,
-                null, null, "");
+        Cursor cursor =
+                getProvider(context).query(
+                        Uri.parse("amigo://patch_info_provider/query_working_checksum"));
 
         String checksum = "";
         if (cursor != null && cursor.moveToFirst()) {
@@ -52,10 +44,8 @@ public class PatchInfoUtil {
     }
 
     public static boolean isDexFileOptimized(Context context, String checksum) {
-        ensureProviderAuthroties(context);
-        Cursor cursor = context.getContentResolver().query(Uri.parse
-                        (URI_PREFIX + "is_dex_optimized?checksum=" + checksum), null,
-                null, null, "");
+        Cursor cursor = getProvider(context).query(Uri.parse
+                ("amigo://patch_info_provider/is_dex_optimized?checksum=" + checksum));
         boolean optimized = false;
         if (cursor != null && cursor.moveToFirst()) {
             optimized = cursor.getInt(0) == 1;
@@ -66,34 +56,33 @@ public class PatchInfoUtil {
     }
 
     public static int updateDexFileOptStatus(Context context, String checksum, boolean optimized) {
-        ensureProviderAuthroties(context);
-        return context.getContentResolver().update(Uri.parse(URI_PREFIX +
-                        "set_dex_optimized?checksum=" + checksum + "&optimized=" + optimized),
-                new ContentValues(0),
-                null, null);
+        return getProvider(context).update(Uri.parse(
+                "amigo://patch_info_provider/set_dex_optimized?checksum="
+                        + checksum
+                        + "&optimized="
+                        + optimized),
+                new ContentValues(0));
     }
 
     public static int updatePatchFileChecksum(Context context, String checksum, Map<String,
             String> checksumMap) {
-        ensureProviderAuthroties(context);
-        return context.getContentResolver().update(Uri.parse
-                        (URI_PREFIX + "update_patch_file_checksum_map?checksum=" + checksum +
-                                "&map=" + toJson(checksumMap)),
-                new ContentValues(0), null, null);
+        final ContentValues contentValues = new ContentValues(0);
+        contentValues.put("checkSumMap", toJson(checksumMap));
+        return getProvider(context)
+                .update(Uri.parse(
+                        "amigo://patch_info_provider/update_patch_file_checksum_map?checksum="
+                                + checksum),
+                        contentValues);
     }
 
     public static void clear(Context context) {
-        ensureProviderAuthroties(context);
-        context.getContentResolver().update(Uri.parse
-                        (URI_PREFIX + "clear_all"), new ContentValues(0), null,
-                null);
+        getProvider(context).update(Uri.parse("amigo://patch_info_provider/clear_all"), null);
     }
 
     public static Map<String, String> getPatchFileChecksum(Context context, String checksum) {
-        ensureProviderAuthroties(context);
-        Cursor cursor = context.getContentResolver().query(Uri.parse
-                        (URI_PREFIX + "query_patch_file_checksum_map?checksum=" + checksum),
-                null, null, null, null);
+        Cursor cursor = getProvider(context).query(
+                Uri.parse("amigo://patch_info_provider/query_patch_file_checksum_map?checksum="
+                        + checksum));
 
         if (cursor == null || !cursor.moveToFirst()) {
             return new HashMap<>(0);
